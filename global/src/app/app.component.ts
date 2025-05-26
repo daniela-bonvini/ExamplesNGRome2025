93e6223d2469f3e0436f5a3fa486d87e1654ac8d
Daniela Bonvini <danbonvini@gmail.com>
2025-05-26 11:29:58 +0000

    moved global store in separate component

--- global/src/app/app.component.ts
+++ global/src/app/app.component.ts
@@ -7,38 +7,37 @@ import { selectCompanions } from './store/companion.selectors';
 import { assignQuest } from './store/companion.actions';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CompanionComponent } from './companion/companion.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, CompanionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private store: Store = inject(Store);
-  companions$: Observable<Companion[]>;
-  selectedCompanionId: number | null = null;
-  questInput: string = '';
-
-  constructor() {
-    this.companions$ = this.store.select(selectCompanions);
-  }
-
-  onAssignQuest() {
-    // Forza il tipo number per selectedCompanionId
-    const id =
-      this.selectedCompanionId !== null
-        ? Number(this.selectedCompanionId)
-        : null;
-    if (id && this.questInput) {
-      console.log(id, this.questInput);
-      this.store.dispatch(
-        assignQuest({
-          id,
-          quest: this.questInput,
-        })
-      );
-      this.questInput = '';
-    }
-  }
+  // private store: Store = inject(Store);
+  // companions$: Observable<Companion[]>;
+  // selectedCompanionId: number | null = null;
+  // questInput: string = '';
+  // constructor() {
+  //   this.companions$ = this.store.select(selectCompanions);
+  // }
+  // onAssignQuest() {
+  //   // Forza il tipo number per selectedCompanionId
+  //   const id =
+  //     this.selectedCompanionId !== null
+  //       ? Number(this.selectedCompanionId)
+  //       : null;
+  //   if (id && this.questInput) {
+  //     console.log(id, this.questInput);
+  //     this.store.dispatch(
+  //       assignQuest({
+  //         id,
+  //         quest: this.questInput,
+  //       })
+  //     );
+  //     this.questInput = '';
+  //   }
+  // }
 }
---
+++ global/src/app/companion/companion.component.ts
@@ -0,0 +1,43 @@
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Companion } from '../models/companion.model';
+import { assignQuest } from '../store/companion.actions';
+import { selectCompanions } from '../store/companion.selectors';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-companion',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './companion.component.html',
+  styleUrl: './companion.component.scss',
+})
+export class CompanionComponent {
+  private store: Store = inject(Store);
+  companions$: Observable<Companion[]>;
+  selectedCompanionId: number | null = null;
+  questInput: string = '';
+
+  constructor() {
+    this.companions$ = this.store.select(selectCompanions);
+  }
+
+  onAssignQuest() {
+    // Forza il tipo number per selectedCompanionId
+    const id =
+      this.selectedCompanionId !== null
+        ? Number(this.selectedCompanionId)
+        : null;
+    if (id && this.questInput) {
+      console.log(id, this.questInput);
+      this.store.dispatch(
+        assignQuest({
+          id,
+          quest: this.questInput,
+        })
+      );
+      this.questInput = '';
+    }
+  }
+}
