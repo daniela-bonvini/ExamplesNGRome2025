aacc17865f449370b46a461fc95c6c177b4fd197
Daniela Bonvini <danbonvini@gmail.com>
2025-06-02 11:47:09 +0000

    added icons for members status

--- global/src/app/fellowship-member-status/status-card/status-card.component.ts
+++ global/src/app/fellowship-member-status/status-card/status-card.component.ts
@@ -1,11 +1,12 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { FellowshipMemberStatus } from '../models/fellowshipMemberStatus.model';
 import { FellowshipMemberStatusStore } from '../store/fellowshipMemberStatus.store';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-status-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './status-card.component.html',
   styleUrl: './status-card.component.scss',
   providers: [FellowshipMemberStatusStore],
@@ -14,9 +15,19 @@ export class StatusCardComponent {
   private componentStore = inject(FellowshipMemberStatusStore);
   member = input.required<FellowshipMemberStatus>({});
 
-  updatedMember = toSignal(
+  memberStatus = toSignal(
     this.componentStore.select((state) => state.memberStatus)
   );
+
+  src = computed(() => {
+    const srcName =
+      this.memberStatus()?.status === 'dead'
+        ? 'skull-crossbones-solid'
+        : this.memberStatus()?.status === 'unknown'
+        ? 'question-solid'
+        : 'face-smile-solid';
+    return `assets/icons/${srcName}.svg`;
+  });
 
   ngOnInit() {
     this.componentStore.setState({
@@ -25,7 +36,7 @@ export class StatusCardComponent {
   }
 
   changeMemberStatus() {
-    if (this.updatedMember()) {
+    if (this.memberStatus()) {
       this.componentStore.setState((state) => {
         return {
           ...state,
